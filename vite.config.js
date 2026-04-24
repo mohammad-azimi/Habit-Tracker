802aef9c2a2f89b4cdf294a3397a5d870b79c895
mohammad-azimi <mohammadzimi1011@gmail.com>
2026-04-24 16:30:15 +0300

    add GitHub Actions workflow for Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/Habit-Tracker/",
   plugins: [react(), tailwindcss()],
 });
